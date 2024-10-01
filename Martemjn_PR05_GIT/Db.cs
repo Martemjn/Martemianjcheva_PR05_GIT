0e0f4c1d08b96b13f88d44fd29386b423d839d1c
Студент251 <student251@zamt.local>
2024-10-01 09:17:05 +0300

    Создан модуль Db.cs со статическим классом

---
+++ Martemjn_PR05_GIT/Db.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martemjn_PR05_GIT
+{
+    static class Db
+    {
+
+    }
+}
